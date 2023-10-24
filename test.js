7e1257b15646594c81838ab83bcb40874ba7892d
Bilal1998a <101061169+Bilal1998a@users.noreply.github.com>
2023-10-24 12:37:56 +0200

    fixed error handling

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
+const x = 10;
+const y = 15
+
+
 function test(){
-    console.log("test running, no issues")
+  if(x == y){  
+  console.log("test running, no issues")
+  } else {
+    throw new Error("numbers are not equal")
 }
+  }
+
 test();
